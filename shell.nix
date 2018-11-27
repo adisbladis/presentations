8820968ff89e37e1f64238960baddfbdbe78dbcd
adisbladis <adisbladis@gmail.com>
2018-11-27 20:39:46 +0000

    Add stockholm emacs meetup presentation

--- shell.nix
+++ shell.nix
@@ -4,8 +4,16 @@ let
  tl = (texlive.combine {
    inherit (texlive) scheme-medium wrapfig ulem capt-of
    titlesec preprint enumitem paralist ctex environ
-   beamer trimspaces zhnumber changepage framed; });
+   beamer trimspaces zhnumber changepage framed
+   fvextra minted upquote ifplatform xstring; });
+ pythonEnv = python3.withPackages(ps: [
+   ps.pygments
+ ]);
 
 in mkShell {
-  buildInputs = [ tl emacs ];
+  buildInputs = [
+    pythonEnv
+    emacs
+    tl
+  ];
 }
